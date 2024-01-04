fa30860ab749a080ae9fdb115cad9d582752aa71
saigo03 <149645624+saigo03@users.noreply.github.com>
2024-01-04 13:57:26 +0000

    テンプレートモーダル実装

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from 'app/javascript/memo', under: 'memo'
+pin_all_from 'app/javascript/template', under: 'template'
 
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
